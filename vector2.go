e885aeb2de9e7f69d6816c99901a030ebce61695
FVT34U <captaintedotron55@gmail.com>
2024-12-25 13:58:38 +0400

    rename vector2 to Vector2

--- vector2.go
+++ vector2.go
@@ -5,42 +5,42 @@ import (
 	"math"
 )
 
-type vector2 struct {
+type Vector2 struct {
 	x float64
 	y float64
 }
 
-func Add(a, b vector2) vector2 {
-	return vector2{a.x + b.x, a.y + b.y}
+func Add(a, b Vector2) Vector2 {
+	return Vector2{a.x + b.x, a.y + b.y}
 }
 
-func Sub(a, b vector2) vector2 {
-	return vector2{a.x - b.x, a.y - b.y}
+func Sub(a, b Vector2) Vector2 {
+	return Vector2{a.x - b.x, a.y - b.y}
 }
 
-func Length(p vector2) float64 {
+func Length(p Vector2) float64 {
 	return math.Sqrt(math.Pow(p.x, 2) + math.Pow(p.y, 2))
 }
 
-func ScalarMul(p vector2, a float64) vector2 {
-	return vector2{p.x * a, p.y * a}
+func ScalarMul(p Vector2, a float64) Vector2 {
+	return Vector2{p.x * a, p.y * a}
 }
 
-func VectorMul(a vector2, b vector2) float64 {
+func VectorMul(a Vector2, b Vector2) float64 {
 	return a.x*b.x + a.y*b.y
 }
 
-func GetAngleBetween2Vectors(a vector2, b vector2) float64 {
+func GetAngleBetween2Vectors(a Vector2, b Vector2) float64 {
 	return math.Acos(VectorMul(a, b)/(Length(a)*Length(b))) * (180.0 / math.Pi)
 }
 
-func RotateVectorByAngle(p vector2, angle float64, isDegrees bool) vector2 {
+func RotateVectorByAngle(p Vector2, angle float64, isDegrees bool) Vector2 {
 	if isDegrees {
 		angle = angle * (math.Pi / 180)
 	}
-	return vector2{p.x*math.Cos(angle) - p.y*math.Sin(angle), p.x*math.Sin(angle) + p.y*math.Cos(angle)}
+	return Vector2{p.x*math.Cos(angle) - p.y*math.Sin(angle), p.x*math.Sin(angle) + p.y*math.Cos(angle)}
 }
 
-func PrintV(p vector2) {
+func PrintV(p Vector2) {
 	fmt.Printf("[%v, %v]", p.x, p.y)
 }
