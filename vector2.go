5ae27544dce0966022a9177d143fe2e49e899d57
FVT34U <captaintedotron55@gmail.com>
2024-12-25 14:10:59 +0400

    change x to X and y to Y

--- vector2.go
+++ vector2.go
@@ -6,28 +6,28 @@ import (
 )
 
 type Vector2 struct {
-	x float64
-	y float64
+	X float64
+	Y float64
 }
 
 func Add(a, b Vector2) Vector2 {
-	return Vector2{a.x + b.x, a.y + b.y}
+	return Vector2{a.X + b.X, a.Y + b.Y}
 }
 
 func Sub(a, b Vector2) Vector2 {
-	return Vector2{a.x - b.x, a.y - b.y}
+	return Vector2{a.X - b.X, a.Y - b.Y}
 }
 
 func Length(p Vector2) float64 {
-	return math.Sqrt(math.Pow(p.x, 2) + math.Pow(p.y, 2))
+	return math.Sqrt(math.Pow(p.X, 2) + math.Pow(p.Y, 2))
 }
 
 func ScalarMul(p Vector2, a float64) Vector2 {
-	return Vector2{p.x * a, p.y * a}
+	return Vector2{p.X * a, p.Y * a}
 }
 
 func VectorMul(a Vector2, b Vector2) float64 {
-	return a.x*b.x + a.y*b.y
+	return a.X*b.X + a.Y*b.Y
 }
 
 func GetAngleBetween2Vectors(a Vector2, b Vector2) float64 {
@@ -38,9 +38,9 @@ func RotateVectorByAngle(p Vector2, angle float64, isDegrees bool) Vector2 {
 	if isDegrees {
 		angle = angle * (math.Pi / 180)
 	}
-	return Vector2{p.x*math.Cos(angle) - p.y*math.Sin(angle), p.x*math.Sin(angle) + p.y*math.Cos(angle)}
+	return Vector2{p.X*math.Cos(angle) - p.Y*math.Sin(angle), p.X*math.Sin(angle) + p.Y*math.Cos(angle)}
 }
 
 func PrintV(p Vector2) {
-	fmt.Printf("[%v, %v]", p.x, p.y)
+	fmt.Printf("[%v, %v]", p.X, p.Y)
 }
